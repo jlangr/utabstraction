4a9368f1b178224221c8f1698e8d9b8a9ad4d952
Jeff Langr <jeff.langr@outpace.com>
2017-02-09 14:12:50 -0700

    removing TODOs

--- java-pi-pantry/src/test/java/com/langrsoft/pi/pantry/AnItemRetriever.java
+++ java-pi-pantry/src/test/java/com/langrsoft/pi/pantry/AnItemRetriever.java
@@ -12,7 +12,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import static com.langrsoft.util.JsonUtil.toJson;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.matches;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,13 +24,14 @@ public class AnItemRetriever {
     @Mock
     HttpClient client;
 
-    // TODO stronger test for URL
     @Test
     public void parsesResponseJsonToItem() {
+        String wheatiesUpc = "0016000275652";
         String responseText = toJson(new ItemBuilder("Wheaties 12oz").create());
-        when(client.retrieveText(anyString())).thenReturn(responseText);
+        when(client.retrieveText(matches("http://.*/json/.*/" + wheatiesUpc)))
+                .thenReturn(responseText);
 
-        Item item = retriever.retrieve("0016000275652");
+        Item item = retriever.retrieve(wheatiesUpc);
 
         assertThat(item.getName(), startsWith("Wheaties"));
     }
--- java-pi-pantry/src/test/java/com/langrsoft/pi/pantry/AnItemRetrieverWithLiveApi.java
+++ java-pi-pantry/src/test/java/com/langrsoft/pi/pantry/AnItemRetrieverWithLiveApi.java
@@ -1,17 +1,19 @@
 package com.langrsoft.pi.pantry;
 
+import com.langrsoft.testutil.Slow;
 import com.langrsoft.util.HttpClient;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 public class AnItemRetrieverWithLiveApi {
-    // TODO mark as slow test?
-    @Ignore("ignore for now: slow integration test")
+    @Ignore("cannot run without valid API key set in property")
+    @Category(Slow.class)
     @Test
-    public void retrievesItemInformationViaHttpGet() {
+    public void retrievesItemInformationViaLiveHttpGet() {
         ItemRetriever retriever = new ItemRetriever(new HttpClient());
         Item item = retriever.retrieve("0016000275652");
         assertThat(item.getName(), startsWith("Wheaties"));
