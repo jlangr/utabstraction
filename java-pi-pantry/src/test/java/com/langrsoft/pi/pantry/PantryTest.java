92c12c9413c7840343c61865b59fae0865eafa23
Jeff Langr <jeff.langr@outpace.com>
2017-04-19 22:03:44 -0400

    java last minute

--- java-pi-pantry/src/test/java/com/langrsoft/pi/pantry/PantryTest.java
+++ java-pi-pantry/src/test/java/com/langrsoft/pi/pantry/PantryTest.java
@@ -1,214 +1,226 @@
 package com.langrsoft.pi.pantry;
 
+import com.langrsoft.util.TestClock;
+import org.junit.Test;
+
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.langrsoft.util.LambdaUtil.map;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import java.time.*;
-import java.util.*;
-import org.junit.Test;
-import com.langrsoft.util.TestClock;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * // Tests for Pantry class
- *
+ * <p>
  * author: I'll never tell
  */
 public class PantryTest {
-   static final LocalDate NOW = LocalDate.now();
+    private static final LocalDate NOW = LocalDate.now();
 
-   private Pantry pantry = new Pantry();
+    private Pantry pantry = new Pantry();
 
-   @Test
-   public void contains1() {
-      try {
-         Pantry pantry = new Pantry();
-         Item i1 = new Item("cheerios");
-         pantry.purchase(i1);
-         assertThat(pantry.contains("cheerios"), equalTo(true));
-      } catch (IllegalArgumentException e) {
-         fail("purchase failed:" + e.getMessage());
-      }
-   }
+    @Test
+    public void contains1() {
+        try {
+            Pantry pantry = new Pantry();
+            Item i1 = new Item("cheerios");
+            pantry.purchase(i1);
+            assertThat(pantry.contains("cheerios"), equalTo(true));
+        } catch (IllegalArgumentException e) {
+            fail("purchase failed:" + e.getMessage());
+        }
+    }
 
-   @Test
-   public void contains2() {
-      try {
-         Pantry pantry = new Pantry();
-         Item cheerios = new Item("cheerios");
-         cheerios.setDescription("cereal");
-         pantry.purchase(cheerios);
-         assertThat(pantry.contains("sugar"), is(equalTo(false)));
-      } catch (IllegalArgumentException e) {
-         fail("purchase failed:" + e.getMessage());
-      }
-   }
+    @Test
+    public void contains2() {
+        try {
+            Pantry pantry = new Pantry();
+            Item cheerios = new Item("cheerios");
+            cheerios.setDescription("cereal");
+            pantry.purchase(cheerios);
+            assertThat(pantry.contains("sugar"), is(equalTo(false)));
+        } catch (IllegalArgumentException e) {
+            fail("purchase failed:" + e.getMessage());
+        }
+    }
 
-   /**
-    * count2
-    *
-    * count of items by name
-    */
-   @Test
-   public void count() {
-      try {
-         Pantry pantry = new Pantry();
-         assertThat(pantry.count("sugar"), is(equalTo(0)));
+    /**
+     * count2
+     * <p>
+     * count of items by name
+     */
+    @Test
+    public void count() {
+        try {
+            Pantry pantry = new Pantry();
+            assertThat(pantry.count("sugar"), is(equalTo(0)));
 
-         pantry.purchase(new Item("sugar"));
+            pantry.purchase(new Item("sugar"));
 
-         pantry.purchase(new Item("cheerios"));
+            pantry.purchase(new Item("cheerios"));
 
-         pantry.purchase(new Item("cheerios"));
-         assertThat("pantry count not correct for cheerioes", pantry.count("cheerios"), is(equalTo(2)));
+            pantry.purchase(new Item("cheerios"));
+            assertThat("pantry count not correct for cheerioes", pantry.count("cheerios"), is(equalTo(2)));
 
-      } catch (IllegalArgumentException e) {
-         fail("purchase failed:" + e.getMessage());
-      }
+        } catch (IllegalArgumentException e) {
+            fail("purchase failed:" + e.getMessage());
+        }
 
-   }
+    }
 
-   /**
-    * count
-    *
-    * count of all items
-    */
-   @Test
-   public void count2() {
-      Pantry pantry = new Pantry();
-      pantry.purchase(new Item("sugar"));
-      pantry.purchase(new Item("sugar"));
-      pantry.purchase(new Item("cheerios"));
-      pantry.purchase(new Item("cheerios"));
-      assertThat("pantry count not 4", pantry.count(), is(equalTo(4)));
-   }
+    /**
+     * count
+     * <p>
+     * count of all items
+     */
+    @Test
+    public void count2() {
+        Pantry pantry = new Pantry();
+        pantry.purchase(new Item("sugar"));
+        pantry.purchase(new Item("sugar"));
+        pantry.purchase(new Item("cheerios"));
+        pantry.purchase(new Item("cheerios"));
+        assertThat("pantry count not 4", pantry.count(), is(equalTo(4)));
+    }
 
-   /**
-    * dt
-    *
-    * ensure purchase date set to today
-    */
-   @Test
-   public void get0() {
-      Pantry pantry = new Pantry();
-      Item sugar = new Item("sugar");
-      sugar.setDescription("refined sweetener");
-      pantry.purchase(sugar);
-      Item retrieved = pantry.getItemNamed("sugar");
-      assertThat("item should not be null", retrieved, is(not(nullValue()))); // null?
-      assertThat(retrieved.getDescription(), is(equalTo("refined sweetener")));
-   }
+    /**
+     * dt
+     * <p>
+     * ensure purchase date set to today
+     */
+    @Test
+    public void get0() {
+        Pantry pantry = new Pantry();
+        Item sugar = new Item("sugar");
+        sugar.setDescription("refined sweetener");
+        pantry.purchase(sugar);
+        Item retrieved = pantry.getItemNamed("sugar");
+        assertThat("item should not be null", retrieved, is(not(nullValue()))); // null?
+        assertThat(retrieved.getDescription(), is(equalTo("refined sweetener")));
+    }
 
-   /**
-    * dt
-    *
-    * ensure purchase date set to today
-    * uses the java clock class as a basis for the fake
-    */
-   @Test
-   public void dt() {
-      LocalDateTime todayAtNoon = LocalDate.now().atTime(12, 0, 0);
-      Instant todayNoonUTC = todayAtNoon.toInstant(ZoneOffset.UTC);
-      Clock clock = Clock.fixed(todayNoonUTC, ZoneOffset.UTC);
-      Pantry pantry = new Pantry();
-      // inject fake clock
-      pantry.setClock(clock);
-      Item eggs = new Item("eggs");
-      eggs.setDescription("oval ovum");
-      pantry.purchase(eggs);
-      Item retrieved = pantry.getItemNamed("eggs");
-      assertThat(retrieved, is(not(nullValue())));
-      assertThat(retrieved.getPurchaseDate(), is(equalTo(LocalDate.now())));
-   }
+    /**
+     * dt
+     * <p>
+     * ensure purchase date set to today
+     * uses the java clock class as a basis for the fake
+     */
+    @Test
+    public void dt() {
+        LocalDateTime todayAtNoon = LocalDate.now().atTime(12, 0, 0);
+        Instant todayNoonUTC = todayAtNoon.toInstant(ZoneOffset.UTC);
+        Clock clock = Clock.fixed(todayNoonUTC, ZoneOffset.UTC);
+        Pantry pantry = new Pantry();
+        // inject fake clock
+        pantry.setClock(clock);
+        Item eggs = new Item("eggs");
+        eggs.setDescription("oval ovum");
+        pantry.purchase(eggs);
+        Item retrieved = pantry.getItemNamed("eggs");
+        assertThat(retrieved, is(not(nullValue())));
+        assertThat(retrieved.getPurchaseDate(), is(equalTo(LocalDate.now())));
+    }
 
-   @Test
-   public void get3() {
-      Pantry pantry = new Pantry();
-      assertThat(pantry.getItemNamed("did not purchase"), is(nullValue()));
-   }
+    @Test
+    public void get3() {
+        Pantry pantry = new Pantry();
+        assertThat(pantry.getItemNamed("did not purchase"), is(nullValue()));
+    }
 
-   @Test
-   public void purchaseNull() {
-      try {
-         Pantry pantry = new Pantry();
-         pantry.purchase(null);
-         fail("should have thrown IllegalArgumentException");
-      } catch (IllegalArgumentException e) {
-         // ok
-      }
-   }
+    @Test
+    public void purchaseNull() {
+        try {
+            Pantry pantry = new Pantry();
+            pantry.purchase(null);
+            fail("should have thrown IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // ok
+        }
+    }
 
-   @Test
-   public void get2() {
-      Pantry pantry = new Pantry();
-      Item smallCoffee = new Item("coffee");
-      smallCoffee.setDescription("small");
-      Item largeCoffee = new Item("coffee");
-      largeCoffee.setDescription("large");
-      pantry.purchase(smallCoffee);
-      pantry.purchase(largeCoffee);
-      List<Item> coffees = pantry.getItemsNamed("coffee");
-      assertThat(coffees.stream().map(Item::getDescription).collect(toList()),
-         equalTo(asList("small", "large")));
-      List<Item> items = pantry.getItemsNamed("pizza");
-      assertThat(items, is(emptyList()));
-   }
+    @Test
+    public void get2() {
+        Pantry pantry = new Pantry();
+        Item smallCoffee = new Item("coffee");
+        smallCoffee.setDescription("small");
+        Item largeCoffee = new Item("coffee");
+        largeCoffee.setDescription("large");
+        pantry.purchase(smallCoffee);
+        pantry.purchase(largeCoffee);
+        List<Item> coffees = pantry.getItemsNamed("coffee");
+        assertThat(coffees.stream().map(Item::getDescription).collect(toList()),
+                equalTo(asList("small", "large")));
+        List<Item> items = pantry.getItemsNamed("pizza");
+        assertThat(items, is(emptyList()));
+    }
 
-   @Test
-   public void expieringToday() {
-      Pantry pantry = new Pantry();
-      LocalDate d = LocalDate.now();
-      pantry.setClock(TestClock.fixedTo(d));
-      Item item1 = new Item("milk");
-      item1.setCategory("milk");
-      item1.setExpirationDate(d);
-      pantry.purchase(item1);
-      // add another item
-      Item item2 = new Item("blood orange juice 24oz");
-      item2.setExpirationDate(d.plusDays(1));
-      item2.setCategory("orange juice");
-      pantry.purchase(item2);
-      // retrieve
-      List<Item> items = pantry.getItemsExpiringToday();
-      List<String> actualItemNames = new ArrayList<>();
-      for (Item i: items) {
-         actualItemNames.add(i.getName());
-      }
-      System.out.println("actual item names:" + actualItemNames);
-      List<String> expectedItemNames = new ArrayList<>();
-      expectedItemNames.add("milk");
-      assertThat(actualItemNames, equalTo(expectedItemNames));
-   }
+    @Test
+    public void expieringToday() {
+        try {
+            Pantry pan = new Pantry();
 
+            LocalDate d = LocalDate.now();
+            Clock clock = TestClock.fixedTo(d);
+            pan.setClock(clock);
 
-   @Test
-   public void allExpiredItemsIncludesItemsExpiringTodayOrEarlier() {
-      pantry.setClock(TestClock.fixedTo(NOW));
-      pantry.purchase(new ItemBuilder("milk").withExpirationDate(NOW).create());
-      pantry.purchase(new ItemBuilder("juice").withExpirationDate(NOW.plusDays(1)).create());
-      pantry.purchase(new ItemBuilder("curdled milk").withExpirationDate(NOW.minusDays(1)).create());
+            Item item1 = new Item("milk");
+            item1.setCategory("milk");
+            item1.setExpirationDate(d);
+            pan.purchase(item1);
+            // add another item
+            String item2Name = "blood orange juice 24oz";
+            Item item2 = new Item(item2Name);
+            item2.setExpirationDate(d.plusDays(1));
+            item2.setCategory("orange juice");
+            pan.purchase(item2);
+            // retrieve
+            List<Item> items = pan.getItemsExpiringToday();
+            assertThat(items, is(notNullValue()));
+            List<String> actualItemNames = new ArrayList<>();
+            for (Item i : items) {
+                actualItemNames.add(i.getName());
+            }
+            System.out.println("actual item names:" + actualItemNames);
+            List<String> expectedItemNames = new ArrayList<>();
+            expectedItemNames.add("milk");
+            assertThat("actual items didn't match", actualItemNames, equalTo(expectedItemNames));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-      List<Item> items = pantry.getAllExpiredItems();
+    @Test
+    public void allExpiredItemsIncludesItemsExpiringTodayOrEarlier() {
+        pantry.setClock(TestClock.fixedTo(NOW));
+        pantry.purchase(new ItemBuilder("milk").withExpirationDate(NOW).create());
+        pantry.purchase(new ItemBuilder("juice").withExpirationDate(NOW.plusDays(1)).create());
+        pantry.purchase(new ItemBuilder("curdled milk").withExpirationDate(NOW.minusDays(1)).create());
 
-      assertThat(map(items, Item::getName),
-         containsInAnyOrder("milk", "curdled milk"));
-   }
+        List<Item> items = pantry.getAllExpiredItems();
 
-   @Test
-   public void tooMany() {
-      Pantry pantry = new Pantry();
-      for (int i = 0; i < 512; i++) {
-         Item item = new Item();
-         pantry.purchase(item);
-      }
-      try {
-         Item item = new Item();
-         pantry.purchase(item);
-         fail("expected exception");
-      } catch (RuntimeException e) {
-         // ok
-      }
-   }
+        assertThat(map(items, Item::getName),
+                containsInAnyOrder("milk", "curdled milk"));
+    }
+
+    @Test
+    public void tooMany() {
+        Pantry pantry = new Pantry();
+        for (int i = 0; i < 512; i++) {
+            Item item = new Item();
+            pantry.purchase(item);
+        }
+        try {
+            Item item = new Item();
+            pantry.purchase(item);
+            fail("expected exception");
+        } catch (RuntimeException e) {
+            // ok
+        }
+    }
 }
